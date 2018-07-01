2da1dfcb832b4d6a9c36c9bd10eae3b362c63fc6
guajiropa <guajiropa@hotmail.com>
2018-06-30 21:43:18 -0400

    30th flask

--- MVA-Trivia/app.py
+++ MVA-Trivia/app.py
@@ -3,7 +3,8 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
@@ -11,9 +12,16 @@ wsgi_app = app.wsgi_app
 
 
 @app.route('/')
-def hello():
+def index():
+    # 
+    list_example = ['Alvin', 'Simon', 'Theodore']
+
     """Renders a sample page."""
-    return "Hello World!"
+    return render_template('index.html', list_example=list_example)
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
 
 if __name__ == '__main__':
     import os
@@ -23,3 +31,5 @@ if __name__ == '__main__':
     except ValueError:
         PORT = 5555
     app.run(HOST, PORT)
+
+
